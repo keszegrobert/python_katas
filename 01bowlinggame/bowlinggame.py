f048c6b892c7edba71a1796a85e99180d8ae7b7b
Robert Keszeg <keszegrobert@gmail.com>
2016-03-20 19:02:26 +0100

    a little refactoring of the score function suggested by Devill

--- 01bowlinggame/bowlinggame.py
+++ 01bowlinggame/bowlinggame.py
@@ -21,17 +21,18 @@ class BowlingGame:
 	def scoreForSpare(self,frame):
 		return self.rolls[frame] + self.rolls[frame+1]
 
+	def getScoreForFrame(self,frame):
+		if self.isStrike(frame):
+			return 10 + self.getBonusForStrike(frame)
+		elif self.isSpare(frame):
+			return 10 + self.getBonusForSpare(frame);
+		else:
+			return self.scoreForSpare(frame)
+
 	def score(self):
 		counter = 0
 		frameIndex = 0
 		for i in range(0,10):
-			if self.isStrike(frameIndex):
-				counter += 10 + self.getBonusForStrike(frameIndex)
-				frameIndex += 1
-			elif self.isSpare(frameIndex):
-				counter += 10 + self.getBonusForSpare(frameIndex);
-				frameIndex += 2
-			else:
-				counter += self.scoreForSpare(frameIndex)
-				frameIndex += 2
+			counter += self.getScoreForFrame(frameIndex)
+			frameIndex += 1 if self.isStrike(frameIndex) else 2
 		return counter
