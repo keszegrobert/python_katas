3fecb7c394a0ef97ff8e1e57343ed1d7e723f52a
Robert Keszeg <keszegrobert@gmail.com>
2016-03-20 12:42:40 +0100

    setup and teardown for the tests in a not so nice way

--- 01bowlinggame/test_bowlinggame.py
+++ 01bowlinggame/test_bowlinggame.py
@@ -1,15 +1,26 @@
 from bowlinggame import BowlingGame
+from nose.tools import with_setup
+
+game = None
 
 #https://github.com/hontas/bowling-game-kata
-
-def test_guttergame():
+def setup_func():
+	global game
 	game = BowlingGame()
-	for i in range(1,20):
+
+def teardown_func():
+	pass
+
+@with_setup(setup_func,teardown_func)
+def test_guttergame():
+	global game
+	for i in range(0,20):
 		game.roll(0)
 	assert game.score() == 0
 
+@with_setup(setup_func,teardown_func)
 def test_gameofones():
-	game = BowlingGame()
+	global game
 	for i in range(0,20):
 		game.roll(1)
 	assert game.score() == 20
