6b4c3d65d8a5480bead3afc819132be517d9dae3
Robert Keszeg <keszegrobert@gmail.com>
2016-03-20 17:06:32 +0100

    introduced the frameindex

--- 01bowlinggame/bowlinggame.py
+++ 01bowlinggame/bowlinggame.py
@@ -5,22 +5,26 @@ class BowlingGame:
 
 	def roll(self,pins):
 		self.rolls.append(pins)
+		if pins == 10:
+			self.rolls.append(0)
 
 	def getBonusForSpare(self,frame):
-		return self.rolls[2*frame+2]
+		return self.rolls[frame+2]
 
 	def isSpare(self,frame):
-		return self.rolls[2*frame] + self.rolls[2*frame+1] == 10
+		return self.rolls[frame] + self.rolls[frame+1] == 10
 
 	def scoreForSpare(self,frame):
-		return self.rolls[2*frame] + self.rolls[2*frame+1]
+		return self.rolls[frame] + self.rolls[frame+1]
 
 	def score(self):
 		counter = 0
-
+		frameIndex = 0
 		for i in range(0,10):
-			if self.isSpare(i):
-				counter += 10 + self.getBonusForSpare(i);
+			if self.isSpare(frameIndex):
+				counter += 10 + self.getBonusForSpare(frameIndex);
+				frameIndex += 2
 			else:
-				counter += self.scoreForSpare(i)
+				counter += self.scoreForSpare(frameIndex)
+				frameIndex += 2
 		return counter
