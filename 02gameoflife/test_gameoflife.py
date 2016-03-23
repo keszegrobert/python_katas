7a249f1b59a5b13637050996473a4d5436db1d26
Robert Keszeg <keszegrobert@gmail.com>
2016-03-23 20:53:18 +0100

    added new test for 2 or 3 neighbours

--- 02gameoflife/test_gameoflife.py
+++ 02gameoflife/test_gameoflife.py
@@ -11,11 +11,21 @@ class TestGameOfLife(TestCase):
 		self.evolve([(0,0),(1,0)])
 		self.assert_dead((1,0))
 
+
+	def test_if_any_cell_with_two_or_three_neighbours_survives(self):
+		self.evolve([      (1,1),(2,1),
+			         (0,2),(1,2)])
+		self.assert_alive((0,2))
+		self.assert_alive((1,2))
+
 	def evolve(self,cells):
 		self.next_generation = self.game.evolve(cells)
 
 	def assert_dead(self,cell):
 		self.assertFalse(cell in self.next_generation)
+
+	def assert_alive(self,cell):
+		self.assertTrue(cell in self.next_generation)
 
 if __name__ == '__main__':
 	unittest.main()
