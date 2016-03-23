33e73904b5c8ad9f3593db4e49b951f54c109a63
Robert Keszeg <keszegrobert@gmail.com>
2016-03-23 21:15:38 +0100

    extracted the function which computes the count of neighbours

--- 02gameoflife/gameoflife.py
+++ 02gameoflife/gameoflife.py
@@ -1,8 +1,12 @@
 class GameOfLife:
 	def evolve(self,cells):
 		next_generation = set()
-		for (x,y) in cells:
-			neighbourcount = x+y
+		for cell in cells:
+			neighbourcount = self.__count_living_neighbours(cell)
 			if neighbourcount == 2 or neighbourcount == 3:
-				next_generation.add((x,y))
+				next_generation.add(cell)
 		return next_generation
+
+	def __count_living_neighbours(self,cell):
+		x,y = cell
+		return x + y
