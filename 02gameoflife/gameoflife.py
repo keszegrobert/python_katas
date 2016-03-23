91341788c3dccf504ff580183e09b96d137ecd48
Robert Keszeg <keszegrobert@gmail.com>
2016-03-23 22:03:59 +0100

    REFACTOR: extracted the method __get_neighbours

--- 02gameoflife/gameoflife.py
+++ 02gameoflife/gameoflife.py
@@ -5,19 +5,25 @@ class GameOfLife:
 			neighbourcount = self.__count_living_neighbours(cell,cells)
 			if neighbourcount == 2 or neighbourcount == 3:
 				next_generation.add(cell)
-			x,y = cell
-			for dx,dy in self.__get_neighbour_offsets():
-				neighbour = (x+dx,y+dy)
-				if neighbour not in cells and self.__count_living_neighbours(neighbour,cells) == 3:
+			for neighbour in self.__get_neighbours(cell):
+				if neighbour not in cells and \
+					self.__count_living_neighbours(neighbour,cells) == 3:
 					next_generation.add(neighbour)
 
 		return next_generation
 
-	def __count_living_neighbours(self,cell,cells):
-		number_of_neighbours = 0
+	def __get_neighbours(self,cell):
+		neighbours = []
 		x,y = cell
 		for dx,dy in self.__get_neighbour_offsets():
 			neighbour = (x+dx,y+dy)
+			neighbours.append(neighbour)
+		return neighbours
+
+	def __count_living_neighbours(self,cell,cells):
+		number_of_neighbours = 0
+		x,y = cell
+		for neighbour in self.__get_neighbours(cell):
 			if neighbour in cells:
 				number_of_neighbours += 1
 		return number_of_neighbours
