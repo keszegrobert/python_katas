6858a925d7c78ab66c4285f700ed3e1f52010ee1
Robert Keszeg <keszegrobert@gmail.com>
2016-03-23 20:09:04 +0100

    test if the gameoflife class can be instantiated

--- 02gameoflife/test_gameoflife.py
+++ 02gameoflife/test_gameoflife.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
 from gameoflife import GameOfLife
 
+class TestGameOfLife(TestCase):
+	def test_if_gameoflife_can_be_instantiated(self):
+		game = GameOfLife()
+
 if __name__ == '__main__':
 	unittest.main()
