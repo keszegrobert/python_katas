e2da3251cab4a123d66a05c020d7b1eb8838a810
Robert Keszeg <keszegrobert@gmail.com>
2016-03-23 21:27:29 +0100

    PASSED all the tests

--- 02gameoflife/gameoflife.py
+++ 02gameoflife/gameoflife.py
@@ -2,11 +2,18 @@ class GameOfLife:
 	def evolve(self,cells):
 		next_generation = set()
 		for cell in cells:
-			neighbourcount = self.__count_living_neighbours(cell)
+			neighbourcount = self.__count_living_neighbours(cell,cells)
 			if neighbourcount == 2 or neighbourcount == 3:
 				next_generation.add(cell)
 		return next_generation
 
-	def __count_living_neighbours(self,cell):
+	def __count_living_neighbours(self,cell,cells):
+		numbber_of_neighbours = 0
 		x,y = cell
-		return x + y
+		for (dx,dy) in [(-1,-1),(-1,0),(-1,1),
+						( 0,-1),       ( 0,1),
+						( 1,-1),( 1,0),( 1,1)]:
+			neighbour = (x+dx,y+dy)
+			if (neighbour in cells):
+				numbber_of_neighbours += 1	
+		return numbber_of_neighbours
