78da9b71809762f27a697448939c5154296511c6
Robert Keszeg <keszegrobert@gmail.com>
2016-03-23 20:34:25 +0100

    added the case of 1 neighbour

--- 02gameoflife/test_gameoflife.py
+++ 02gameoflife/test_gameoflife.py
@@ -6,8 +6,10 @@ class TestGameOfLife(TestCase):
 		self.game = GameOfLife()
 
 	def test_if_any_cell_with_less_than_two_neighbours_dies(self):
-		next_generation = self.game.evolve([(1,1)])
-		self.assertFalse((1,1) in next_generation)
+		next_generation = self.game.evolve([(0,0)])
+		self.assertFalse((0,0) in next_generation)
+		next_generation = self.game.evolve([(0,0),(1,0)])
+		self.assertFalse((1,0) in next_generation)
 
 if __name__ == '__main__':
 	unittest.main()
