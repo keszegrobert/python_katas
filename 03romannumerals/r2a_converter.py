cde22aae87cf2f6051d8cb3cb3c9d110bfe6074d
Robert Keszeg <keszegrobert@gmail.com>
2016-03-24 20:50:48 +0100

    added tests for 4 and invalid input

--- 03romannumerals/r2a_converter.py
+++ 03romannumerals/r2a_converter.py
@@ -1,3 +1,33 @@
 class RomanToArabicConverter:
+	STATE_ERR = -1
+	STATE_0 = 0
+	STATE_I = 1
+	STATE_II = 2
+	STATE_IV = 3
+	STATE_V = 4
+
 	def convert(self,roman):
-		return len(roman)
+		self.state = self.STATE_0
+		result = 0
+		for i in range(0,len(roman)):
+			if self.state == self.STATE_0:
+				if roman[i] == 'I':
+					self.state = self.STATE_I
+					result = 1
+			elif self.state == self.STATE_I:
+				if roman[i] == 'I':
+					self.state = self.STATE_II
+					result = 2
+				elif roman[i] == 'V':
+					self.state = self.STATE_II
+					result = 4
+			elif self.state == self.STATE_II:
+				if roman[i] == 'I':
+					self.state = self.STATE_II
+					result += 1
+				elif roman[i] == 'V':
+					self.state = self.STATE_ERR
+		return result
+
+	def is_valid(self):
+		self.state != self.STATE_ERR
--- 03romannumerals/test_r2a_converter.py
+++ 03romannumerals/test_r2a_converter.py
@@ -7,19 +7,27 @@ class TestRomanToArabicConverter(TestCase):
 
 	def test_i_is_one(self):
 		arabic = self.convert('I')
-		self.assertTrue(arabic == 1)
+		self.assertEquals(arabic,1)
 
 	def test_ii_is_two(self):
 		arabic = self.convert('II')
-		self.assertTrue(arabic == 2)
+		self.assertEquals(arabic,2)
 
 	def test_iii_is_three(self):
 		arabic = self.convert('III')
-		self.assertTrue(arabic == 3)
+		self.assertEquals(arabic,3)
 
 	def test_iiii_is_four(self):
 		arabic = self.convert('IIII')
-		self.assertTrue(arabic == 4)
+		self.assertEquals(arabic,4)
+
+	def test_iv_is_four(self):
+		arabic = self.convert('IV')
+		self.assertEquals(arabic,4)
+
+	def test_iiv_is_invalid(self):
+		arabic = self.convert('IIV')
+		self.assertFalse(self.converter.is_valid())
 
 	def convert(self,roman):
 		return self.converter.convert(roman)
