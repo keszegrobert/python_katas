de12ffbd0dffefe2538f9388a579a4a9a8e8e033
Robert Keszeg <keszegrobert@gmail.com>
2016-03-24 19:56:30 +0100

    added the kata roman numerals

---
+++ 03romannumerals/test_r2a_converter.py
@@ -0,0 +1,9 @@
+from unittest import TestCase 
+from r2a_converter import RomanToArabicConverter
+
+class TestRomanToArabicConverter(TestCase):
+	def setUp(self):
+		self.converter = RomanToArabicConverter()
+
+if __name__ == '__main__':
+	unittest.main()
