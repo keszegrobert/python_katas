d34bd310eb3045c61d34780c3f13cab3e0495d1a
Robert Keszeg <keszegrobert@gmail.com>
2016-03-25 20:11:29 +0100

    tested every roman number from 0 to 999

--- 03romannumerals/r2a_converter.py
+++ 03romannumerals/r2a_converter.py
@@ -20,28 +20,46 @@ SLXX = 70
 SLXXX = 80
 SXC = 90
 SC = 100
+SCC = 200
+SCCC = 300
+SCD = 400
+SD = 500
+SDC = 600
+SDCC = 700
+SDCCC = 800
+SCM = 900
+SM = 1000
 
 class RomanToArabicConverter:
 	state_machine = {
-		S0:   {'I':(SI,1),   'V':(SV,5), 'X':(SX,10), 'L':(SL,50) },
-		SI:   {'I':(SII,1),  'V':(SIV,3),'X':(SIX,8) },
-		SII:  {'I':(SIII,1) 			 },
-		SIII: {'I':(SIV,1)               },
-		SIV:  {                          },
-		SV:   {'I':(SVI,1)               },
-		SVI:  {'I':(SVII,1)              },
-		SVII: {'I':(SVIII,1)             },
+		S0:   {'I':(SI,1), 'V':(SV,5), 'X':(SX,10), 'L':(SL,50), 'C':(SC,100),'D':(SD,500) },
+		SI:   {'I':(SII,1),'V':(SIV,3),'X':(SIX,8), },
+		SII:  {'I':(SIII,1)},
+		SIII: {'I':(SIV,1)},
+		SIV:  {},
+		SV:   {'I':(SVI,1)},
+		SVI:  {'I':(SVII,1)},
+		SVII: {'I':(SVIII,1)},
 		SVIII:{},
 		SIX:  {},
-		SX:   {'I':(SI,1),   'V':(SV,5), 'X':(SXX,10), 'L':(SXL,30), 'C':(SXC,80) },
-		SXX:  {'I':(SI,1),   'V':(SV,5), 'X':(SXXX,10) },
-		SXXX: {'I':(SI,1),   'V':(SV,5), 'X':(SXL,10) },
-		SXL:  {'I':(SI,1),   'V':(SV,5) },
-		SL:	  {'I':(SI,1),   'V':(SV,5), 'X':(SLX,10)},
-		SLX:  {'I':(SI,1),   'V':(SV,5), 'X':(SLXX,10)},
-		SLXX: {'I':(SI,1),   'V':(SV,5), 'X':(SLXXX,10)},
-		SLXXX:{'I':(SI,1),   'V':(SV,5)},
-		SXC:  {'I':(SI,1),   'V':(SV,5)}
+		SX:   {'I':(SI,1),'V':(SV,5),'X':(SXX,10), 'L':(SXL,30), 'C':(SXC,80) },
+		SXX:  {'I':(SI,1),'V':(SV,5),'X':(SXXX,10) },
+		SXXX: {'I':(SI,1),'V':(SV,5),'X':(SXL,10) },
+		SXL:  {'I':(SI,1),'V':(SV,5)},
+		SL:	  {'I':(SI,1),'V':(SV,5),'X':(SLX,10)},
+		SLX:  {'I':(SI,1),'V':(SV,5),'X':(SLXX,10)},
+		SLXX: {'I':(SI,1),'V':(SV,5),'X':(SLXXX,10)},
+		SLXXX:{'I':(SI,1),'V':(SV,5),'X':(SXC,10)},
+		SXC:  {'I':(SI,1),'V':(SV,5)},
+		SC:   {'I':(SI,1),'V':(SV,5),'X':(SX,10),'L':(SL,50),'C':(SCC,100),'D':(SCD,300),'M':(SCM,800)},
+		SCC:  {'I':(SI,1),'V':(SV,5),'X':(SX,10),'L':(SL,50),'C':(SCCC,100)},
+		SCCC: {'I':(SI,1),'V':(SV,5),'X':(SX,10),'L':(SL,50),'C':(SCD,100)},
+		SCD:  {'I':(SI,1),'V':(SV,5),'X':(SX,10),'L':(SL,50)},
+		SD:   {'I':(SI,1),'V':(SV,5),'X':(SX,10),'L':(SL,50),'C':(SDC,100)},
+		SDC:  {'I':(SI,1),'V':(SV,5),'X':(SX,10),'L':(SL,50),'C':(SDCC,100)},
+		SDCC: {'I':(SI,1),'V':(SV,5),'X':(SX,10),'L':(SL,50),'C':(SDCCC,100)},
+		SDCCC:{'I':(SI,1),'V':(SV,5),'X':(SX,10),'L':(SL,50),'C':(SCM,100)},
+		SCM:  {'I':(SI,1),'V':(SV,5),'X':(SX,10),'L':(SL,50)},
 	}
 
 
--- 03romannumerals/test_r2a_converter.py
+++ 03romannumerals/test_r2a_converter.py
@@ -13,50 +13,85 @@ class TestRomanToArabicConverter(TestCase):
 		self.check_equality(prefix+'II',add+2)
 		self.check_invalidity(prefix+'IIV')
 		self.check_invalidity(prefix+'IIX')
+		self.check_invalidity(prefix+'IIL')
+		self.check_invalidity(prefix+'IIC')
+		self.check_invalidity(prefix+'IID')
+		self.check_invalidity(prefix+'IIM')
 
 		self.check_equality(prefix+'III',add+3)
 		self.check_invalidity(prefix+'IIIV')
 		self.check_invalidity(prefix+'IIIX')
+		self.check_invalidity(prefix+'IIIL')
+		self.check_invalidity(prefix+'IIIC')
+		self.check_invalidity(prefix+'IIID')
+		self.check_invalidity(prefix+'IIIM')
 
 		self.check_equality(prefix+'IIII',add+4)
 		self.check_invalidity(prefix+'IIIII')
 		self.check_invalidity(prefix+'IIIIV')
 		self.check_invalidity(prefix+'IIIIX')
+		self.check_invalidity(prefix+'IIIIL')
+		self.check_invalidity(prefix+'IIIIC')
+		self.check_invalidity(prefix+'IIIID')
+		self.check_invalidity(prefix+'IIIIM')
 
 		self.check_equality(prefix+'IV',add+4)
 		self.check_invalidity(prefix+'IVI')
 		self.check_invalidity(prefix+'IVV')
 		self.check_invalidity(prefix+'IVX')
+		self.check_invalidity(prefix+'IVL')
+		self.check_invalidity(prefix+'IVC')
+		self.check_invalidity(prefix+'IVD')
+		self.check_invalidity(prefix+'IVM')
 
 		self.check_equality(prefix+'V',add+5)
 		self.check_invalidity(prefix+'VV')
 		self.check_invalidity(prefix+'VX')
+		self.check_invalidity(prefix+'VL')
+		self.check_invalidity(prefix+'VC')
+		self.check_invalidity(prefix+'VD')
+		self.check_invalidity(prefix+'VM')
 
 		self.check_equality(prefix+'VI',add+6)
 		self.check_invalidity(prefix+'VIV')
 		self.check_invalidity(prefix+'VIX')
+		self.check_invalidity(prefix+'VIL')
+		self.check_invalidity(prefix+'VIC')
+		self.check_invalidity(prefix+'VID')
+		self.check_invalidity(prefix+'VIM')
 
 		self.check_equality(prefix+'VII',add+7)
 		self.check_invalidity(prefix+'VIIV')
 		self.check_invalidity(prefix+'VIIX')
+		self.check_invalidity(prefix+'VIIL')
+		self.check_invalidity(prefix+'VIIC')
+		self.check_invalidity(prefix+'VIID')
+		self.check_invalidity(prefix+'VIIM')
 
 		self.check_equality(prefix+'VIII',add+8)
 		self.check_invalidity(prefix+'VIIII')
 		self.check_invalidity(prefix+'VIIIV')
 		self.check_invalidity(prefix+'VIIIX')
+		self.check_invalidity(prefix+'VIIIL')
+		self.check_invalidity(prefix+'VIIIC')
+		self.check_invalidity(prefix+'VIIID')
+		self.check_invalidity(prefix+'VIIIM')
 
 		self.check_equality(prefix+'IX',add+9)
 		self.check_invalidity(prefix+'IXI')
 		self.check_invalidity(prefix+'IXV')
 		self.check_invalidity(prefix+'IXX')
+		self.check_invalidity(prefix+'IXL')
+		self.check_invalidity(prefix+'IXC')
+		self.check_invalidity(prefix+'IXD')
+		self.check_invalidity(prefix+'IXM')
 
-	def test_0_to_9(self):
-		self.check_0_to_9('',0)
-
-	def check_10_to_99(self,prefix,add):
+	def check_0_to_99(self,prefix,add):
+		self.check_0_to_9(prefix+'',add+0)
 		self.check_0_to_9(prefix+'X',add+10)
 		self.check_0_to_9(prefix+'XX',add+20)
 		self.check_0_to_9(prefix+'XXX',add+30)
+		self.check_0_to_9(prefix+'XXXX',add+40)
 		self.check_0_to_9(prefix+'XL',add+40)
 		self.check_0_to_9(prefix+'L',add+50)
 		self.check_0_to_9(prefix+'LX',add+60)
@@ -64,8 +99,21 @@ class TestRomanToArabicConverter(TestCase):
 		self.check_0_to_9(prefix+'LXXX',add+80)
 		self.check_0_to_9(prefix+'XC',add+90)
 
-	def test_10_to_99(self):
-		self.check_10_to_99('',0)
+	def check_0_to_999(self,prefix,add):
+		self.check_0_to_99(prefix+'',add+0)
+		self.check_0_to_99(prefix+'C',add+100)
+		self.check_0_to_99(prefix+'CC',add+200)
+		self.check_0_to_99(prefix+'CCC',add+300)
+		self.check_0_to_99(prefix+'CCCC',add+400)
+		self.check_0_to_99(prefix+'CD',add+400)
+		self.check_0_to_99(prefix+'D',add+500)
+		self.check_0_to_99(prefix+'DC',add+600)
+		self.check_0_to_99(prefix+'DCC',add+700)
+		self.check_0_to_99(prefix+'DCCC',add+800)
+		self.check_0_to_99(prefix+'CM',add+900)
+
+	def test_0_to_999(self):
+		self.check_0_to_999('',0)
 
 	def check_equality(self,roman,arabic):
 		converted = self.converter.convert(roman)
