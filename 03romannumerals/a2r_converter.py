3f003484183576dab2b052b42a728cb70a5d36e5
Robert Keszeg <keszegrobert@gmail.com>
2016-03-25 21:00:14 +0100

    added arabic to roman converter

---
+++ 03romannumerals/a2r_converter.py
@@ -0,0 +1,2 @@
+class ArabicToRomanConverter:
+	pass
---
+++ 03romannumerals/test_a2r_converter.py
@@ -0,0 +1,9 @@
+from a2r_converter import ArabicToRomanConverter
+from unittest import TestCase
+
+class TestArabicToRomanConverter(TestCase):
+	def setUp():
+		self.converter = ArabicToRomanConverter()
+
+if __name__ == '__main__':
+	unittest.main()
