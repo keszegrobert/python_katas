e98e5209dd944289378533f34eba703a5cbc9d97
Robert Keszeg <keszegrobert@gmail.com>
2016-03-26 16:38:41 +0100

    added a forward-backward test

---
+++ 03romannumerals/test_convert.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from r2a_converter import RomanToArabicConverter
+from a2r_converter import ArabicToRomanConverter
+
+
+class TestConvert(TestCase):
+	def setUp(self):
+		self.r2a = RomanToArabicConverter()
+		self.a2r = ArabicToRomanConverter()
+
+	def test_0_to_10(self):
+		for number in range(0,5000):
+			roman = self.a2r.convert(number)
+			arabic = self.r2a.convert(roman)
+			self.assertEqual(arabic,number)
+			roman2 = self.a2r.convert(number)
+			self.assertEqual(roman,roman2)
+
+if __name__ == '__main__':
+	unittest.main()
