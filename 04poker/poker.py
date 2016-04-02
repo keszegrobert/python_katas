d1fcf545b7ab0569b972ab3bdb04ff95d8adf2fb
Robert Keszeg <keszegrobert@gmail.com>
2016-04-02 14:49:53 +0200

    added test case for flush

--- 04poker/poker.py
+++ 04poker/poker.py
@@ -11,14 +11,23 @@ class PokerHand:
 	def __init__(self,cards):
 		self.cards = cards
 
-	def __count_colors(self,number):
+	def __count_cards_of_number(self,number):
 		sum = 0;
 		for color in COLORS:
 			sum += self.cards.count((color,number))
 		return sum
 
+	def __count_cards_of_color(self,color):
+		sum = 0;
+		for number in NUMBERS:
+			sum += self.cards.count((color,number))
+		return sum
+
 	def __count_numbers(self):
-		return [self.__count_colors(number) for number in NUMBERS]
+		return [self.__count_cards_of_number(number) for number in NUMBERS]
+
+	def __count_colors(self):
+		return [self.__count_cards_of_color(color) for color in COLORS]
 
 	def has_pair(self):
 		return 2 in self.__count_numbers()
@@ -35,6 +44,9 @@ class PokerHand:
 		for i in range(0, len(numbers)):
 			groups += str(numbers[i])
 		return groups.find('11111') > -1
+
+	def has_flush(self):
+		return 5 in self.__count_colors()
 
 	def has_poker(self):
 		return 4 in self.__count_numbers()
--- 04poker/test_poker.py
+++ 04poker/test_poker.py
@@ -6,10 +6,11 @@ SAMPLE_PAIR =		[('C','2'),('D','2'),('S','3'),('S','Q'),('C','9')]
 SAMPLE_TWOPAIRS =	[('C','2'),('D','2'),('S','3'),('C','3'),('C','9')]
 SAMPLE_DRILL =		[('C','2'),('D','2'),('S','2'),('S','Q'),('C','9')]
 SAMPLE_STRAIGHT =	[('C','7'),('D','8'),('S','9'),('H','10'),('C','J')]
+SAMPLE_FLUSH =		[('C','3'),('C','8'),('C','10'),('C','4'),('C','Q')]
 SAMPLE_POKER =		[('C','Q'),('D','Q'),('S','Q'),('H','Q'),('C','9')]
 
 SAMPLES = [SAMPLE_HIGH,SAMPLE_PAIR,SAMPLE_TWOPAIRS,
-	SAMPLE_DRILL,SAMPLE_STRAIGHT,SAMPLE_POKER
+	SAMPLE_DRILL,SAMPLE_STRAIGHT,SAMPLE_POKER,SAMPLE_FLUSH
 ]
 
 class TestPokerHand(TestCase):
@@ -17,29 +18,31 @@ class TestPokerHand(TestCase):
 		pass
 
 	def test_hand_has_pair(self):
-		expected = [False,True,True,False,False,False]
+		expected = [False,True,True,False,False,False,False]
 		actual = [PokerHand(sample).has_pair() for sample in SAMPLES]
 		self.assertEqual(expected,actual)
 
 	def test_hand_has_two_pairs(self):
-		expected = [False,False,True,False,False,False]
+		expected = [False,False,True,False,False,False,False]
 		actual = [PokerHand(sample).has_two_pairs() for sample in SAMPLES]
 		self.assertEqual(expected,actual)
 
 	def test_hand_has_set(self):
-		expected = [False,False,False,True,False,False]
+		expected = [False,False,False,True,False,False,False]
 		actual = [PokerHand(sample).has_set() for sample in SAMPLES]
 		self.assertEqual(expected,actual)
 
 	def test_hand_has_straight(self):
-		expected = [False,False,False,False,True,False]
+		expected = [False,False,False,False,True,False,False]
 		actual = [PokerHand(sample).has_straight() for sample in SAMPLES]
 		self.assertEqual(expected,actual)
 
-	def test_hand_has_poker(self):
-		expected = [False,False,False,False,False,True]
-		actual = [PokerHand(sample).has_poker() for sample in SAMPLES]
+	def test_hand_has_flush(self):
+		expected = [False,False,False,False,False,False,True]
+		actual = [PokerHand(sample).has_flush() for sample in SAMPLES]
 		self.assertEqual(expected,actual)
 
-
-
+	def test_hand_has_poker(self):
+		expected = [False,False,False,False,False,True,False]
+		actual = [PokerHand(sample).has_poker() for sample in SAMPLES]
+		self.assertEqual(expected,actual)
